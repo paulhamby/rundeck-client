d47fd94f9dcf54c7f016b1a32c07c28c2cd76e2a
Paul Hamby <paul.hamby@bancvue.com>
2015-06-06 15:53:03 -0500

    adding colorized output to getexecutionoutput

--- cmd/get-execution-output.go
+++ cmd/get-execution-output.go
@@ -2,8 +2,7 @@ package cmd
 
 import (
 	"fmt"
-	"github.com/olekukonko/tablewriter"
-	"os"
+	"github.com/mgutz/ansi"
 	//"github.com/lusis/go-rundeck/src/rundeck.v12"
 	. "github.com/paulhamby/go-rundeck/src/rundeck.v12"
 )
@@ -11,19 +10,23 @@ import (
 func GetExecutionOutput(executionid string) {
 	client := NewClientFromEnv()
 	data, err := client.GetExecutionOutput(executionid)
+	red := ansi.ColorCode("red")
+	green := ansi.ColorCode("green")
+	cyan := ansi.ColorCode("cyan")
+	reset := ansi.ColorCode("reset")
 
 	if err != nil {
 		fmt.Printf("%s\n", err)
 	} else {
-		table := tablewriter.NewWriter(os.Stdout)
-		table.SetHeader([]string{
-			"Time",
-			"Node",
-			"Log",
-		})
 		for _, e := range data.Entries.Entry {
-			table.Append([]string{e.Time, e.Node, e.Log})
+			fmt.Printf("%s %s", e.Time, e.Node)
+			if e.Level == "ERROR" {
+				fmt.Println(red, e.Level, e.Log, reset)
+			} else if e.Level == "VERBOSE" {
+				fmt.Println(cyan,e.Level, e.Log, reset)
+			} else {
+				fmt.Println(green,e.Level, e.Log, reset)
+			}
 		}
-		table.Render()
 	}
 }
