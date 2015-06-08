b9310eb6b64f8d5dc468cdbfcab0406c4d1fb3ef
Paul Hamby <paul.hamby@bancvue.com>
2015-06-08 13:30:35 -0500

    adding LogLevel

--- cmd/run-job.go
+++ cmd/run-job.go
@@ -22,19 +22,28 @@ func RunJob(projectid string, jobname string, options string) {
 	//input is quoted, comma-separated key/pairs: option1=option1,option2=option2
 	//output should be : '-option1 option1 -option2 option2'
 	var arguments string
+	var loglevel string
 	if options != "" {
 		j := strings.Split(options, ",")
 		for _, p := range j {
 			s := strings.Split(p, "=")
 			k, v := s[0], s[1]
-			k = "-" + k
-			arguments = arguments + " " + k + " " + v
+			if k == "LogLevel" {
+				loglevel = v
+			} else {
+				k = "-" + k
+				arguments = arguments + " " + k + " " + v
+			}
 		}
 	} else {
 		arguments = ""
 	}
 
-	o := rundeck.RunOptions{LogLevel: "INFO", AsUser: "", Arguments: arguments}
+	if loglevel == "" {
+		loglevel = "INFO"
+	}
+
+	o := rundeck.RunOptions{LogLevel: loglevel, AsUser: "", Arguments: arguments}
 	data, err := client.RunJob(jobID, o)
 	if err != nil {
 		fmt.Printf("%s\n", err)
